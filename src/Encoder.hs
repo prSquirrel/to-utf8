2bb4d90342215d110e9ae174132a9e37a9ea4596
Aleksej Kazmin <prSquirrel@users.noreply.github.com>
2016-10-01 13:57:02 +0300

    Add codepoints < 65536

--- src/Encoder.hs
+++ src/Encoder.hs
@@ -9,7 +9,15 @@ import Data.Bits
 --     AND: 1101 1111 1011 1111 (0xDFBF)
 --      OR: 1100 0000 1000 0000 (0xC080)
 
+
+-- Pattern: 1110 xxxx 10xx xxxx 10xx xxxx
+--
+--     AND: 1110 1111 1011 1111 1011 1111 (0xEFBFBF)
+--      OR: 1110 0000 1000 0000 1000 0000 (0xE08080)
+
+
 encodeUtf8 :: Int -> Int
 encodeUtf8 x
-            | x < 128 = x
-            | otherwise = (x .&. 0xDFBF) .|. 0xC080
+            | x <= 0x7F = x
+            | x <= 0x7FF = (x .&. 0xDFBF) .|. 0xC080
+            | x <= 0xFFFF = (x .&. 0xEFBFBF) .|. 0xE08080
--- test/EncoderSpec.hs
+++ test/EncoderSpec.hs
@@ -13,15 +13,30 @@ gen7Bits = choose (0, 0x7F)
 gen11Bits :: Gen Int
 gen11Bits = choose (0x80, 0x7FF)
 
+gen16Bits :: Gen Int
+gen16Bits = choose (0x800, 0xFFFF)
+
 spec :: Spec
 spec =
   describe "encodeUtf8" $ do
-    it "has direct mapping for codes [0, 127]" $ quickCheckWith stdArgs { maxSuccess = 1000 } $ property $
+    it "has direct mapping for codes [0, 127]" $ quickCheckWith stdArgs { maxSuccess = 400 } $ property $
       forAll gen7Bits $ \x -> encodeUtf8 x == x
-    it "adheres to pattern 0xxxxxxx for codes [0, 127]" $ quickCheckWith stdArgs { maxSuccess = 1000 } $ property $ do
-      let firstBit x = x .&. 0x80 -- mask: 1000 0000
+
+    it "adheres to pattern 0xxx xxxx for codes [0, 127]" $ quickCheckWith stdArgs { maxSuccess = 400 } $ property $ do
+      -- 1000 0000
+      let firstBit x = x .&. 0x80
       forAll gen7Bits $ \x -> (firstBit . encodeUtf8) x == 0
-    it "adheres to pattern 110xxxxx 10xxxxxx for codes [128, 2047]" $ quickCheckWith stdArgs { maxSuccess = 10000 } $ property $ do
-      let bitMask x = x .&. 0xE0C0 -- mask: 1110 0000 1100 0000
-      let pattern = 0xC080 -- 110x xxxx 10xx xxxx
+
+    it "adheres to pattern 110xx xxx 10xx xxxx for codes [128, 2047]" $ quickCheckWith stdArgs { maxSuccess = 4000 } $ property $ do
+      -- 1110 0000 1100 0000
+      let bitMask x = x .&. 0xE0C0
+      -- 110x xxxx 10xx xxxx
+      let pattern = 0xC080
       forAll gen11Bits $ \x -> (bitMask . encodeUtf8) x == pattern
+
+    it "adheres to pattern 1110 xxxx 10xx xxxx 10xx xxxx for codes [2048, 65535]" $ quickCheckWith stdArgs { maxSuccess = 120000 } $ property $ do
+      -- 1111 0000 1100 0000 1100 0000
+      let bitMask x = x .&. 0xF0C0C0
+      -- 1110 xxxx 10xx xxxx 10xx xxxx
+      let pattern = 0xE08080
+      forAll gen16Bits $ \x -> (bitMask . encodeUtf8) x == pattern
