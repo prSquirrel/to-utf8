28d552a7a64a9afdf35c3549a26b92d8240f38e2
Aleksej Kazmin <prSquirrel@users.noreply.github.com>
2016-10-01 13:16:03 +0300

    Add Code points of 2 bytes or less

--- src/Encoder.hs
+++ src/Encoder.hs
@@ -1,4 +1,15 @@
 module Encoder where
 
-encodeUtf8 :: Int -> Char
-encodeUtf8 x = '\0'
+import Data.Char
+import Data.Bits
+
+
+-- Pattern: 110x xxxx 10xx xxxx
+--
+--     AND: 1101 1111 1011 1111 (0xDFBF)
+--      OR: 1100 0000 1000 0000 (0xC080)
+
+encodeUtf8 :: Int -> Int
+encodeUtf8 x
+            | x < 128 = x
+            | otherwise = (x .&. 0xDFBF) .|. 0xC080
--- test/EncoderSpec.hs
+++ test/EncoderSpec.hs
@@ -2,10 +2,26 @@ module EncoderSpec (spec) where
 
 import           Encoder
 import           Test.Hspec
-import Test.QuickCheck
+import Test.QuickCheck hiding ((.&.))
+
+import Data.Char
+import Data.Bits
+
+gen7Bits :: Gen Int
+gen7Bits = choose (0, 0x7F)
+
+gen11Bits :: Gen Int
+gen11Bits = choose (0x80, 0x7FF)
 
 spec :: Spec
 spec =
-  describe "Encoder" $
-    it "should work" $
-      encodeUtf8 0 `shouldBe` '\0'
+  describe "encodeUtf8" $ do
+    it "has direct mapping for codes [0, 127]" $ quickCheckWith stdArgs { maxSuccess = 1000 } $ property $
+      forAll gen7Bits $ \x -> encodeUtf8 x == x
+    it "adheres to pattern 0xxxxxxx for codes [0, 127]" $ quickCheckWith stdArgs { maxSuccess = 1000 } $ property $ do
+      let firstBit x = x .&. 0x80 -- mask: 1000 0000
+      forAll gen7Bits $ \x -> (firstBit . encodeUtf8) x == 0
+    it "adheres to pattern 110xxxxx 10xxxxxx for codes [128, 2047]" $ quickCheckWith stdArgs { maxSuccess = 10000 } $ property $ do
+      let bitMask x = x .&. 0xE0C0 -- mask: 1110 0000 1100 0000
+      let pattern = 0xC080 -- 110x xxxx 10xx xxxx
+      forAll gen11Bits $ \x -> (bitMask . encodeUtf8) x == pattern
