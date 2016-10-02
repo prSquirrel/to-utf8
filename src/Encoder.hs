3695ed6f46fdf849fa1af48801b8461952c7ff74
Aleksej Kazmin <prSquirrel@users.noreply.github.com>
2016-10-02 12:37:02 +0300

    Refactor

--- src/Encoder.hs
+++ src/Encoder.hs
@@ -6,14 +6,19 @@ encodeUtf8 :: Int -> Int
 encodeUtf8 x
     | x <= 0x7F = x
     | x <= 0x7FF = 0xC080
-          .|. ((x `shiftR` 6) `shiftL` 8)
-          .|. (x .&. 0x3F)
+          .|. maskByte1 x
+          .|. maskByte2 x `shiftL` 8
     | x <= 0xFFFF = 0xE08080
-          .|. (x .&. 0x3F)
-          .|. (((x `shiftR` 6) .&. 0x3F) `shiftL` 8)
-          .|. ((x `shiftR` 12) `shiftL` 16)
+          .|. maskByte1 x
+          .|. maskByte2 x `shiftL` 8
+          .|. maskByte3 x `shiftL` 16
     | x <= 0x10FFFF = 0xF0808080
-          .|. (x .&. 0x3F)
-          .|. (((x `shiftR` 6) .&. 0x3F) `shiftL` 8)
-          .|. (((x `shiftR` 12) .&. 0x3F) `shiftL` 16)
-          .|. ((x `shiftR` 18) `shiftL` 24)
+          .|. maskByte1 x
+          .|. maskByte2 x `shiftL` 8
+          .|. maskByte3 x `shiftL` 16
+          .|. maskByte4 x `shiftL` 24
+  where
+    maskByte1 x = x .&. 0x3F
+    maskByte2 x = maskByte1 $ x `shiftR` 6
+    maskByte3 x = maskByte1 $ x `shiftR` 12
+    maskByte4 x = x `shiftR` 18
